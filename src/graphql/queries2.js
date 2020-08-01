e6d09cf9e1b49deb37565012cca7f239ab4b10b4
gmart <martinetti.gregoire@gmail.com>
2020-08-01 15:23:46 +0200

    add queries

---
+++ src/graphql/queries2.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getTodoBis = /* GraphQL */ `
+  query GetTodo($id: ID!) {
+    getTodo(id: $id) {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listTodosBis = /* GraphQL */ `
+  query ListTodos(
+    $filter: ModelTodoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        name
+        description
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
