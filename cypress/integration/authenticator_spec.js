295adc5cf1d956932049e95546d2054a2ec9dd46
gmart <martinetti.gregoire@gmail.com>
2020-08-02 12:45:01 +0200

    add cypress test test

---
+++ cypress/integration/authenticator_spec.js
@@ -0,0 +1,20 @@
+describe('Authenticator:', function () {
+  // Step 1: setup the application state
+  beforeEach(function () {
+    cy.visit('/');
+  });
+
+  describe('Sign In:', () => {
+    it('allows a user to signin', () => {
+      // Step 2: Take an action (Sign in)
+      cy.get('input[name="username"]').type("Tilko");
+      cy.get('input[name="password"]').type("J8uXs25=");
+      cy.get('button').contains('Sign In').click();
+
+      // Step 3: Make an assertion (Check for sign-out text)
+      //cy.get('button').contains('OUT', { matchCase: false });
+      cy.get('button')
+        .should('have.text', 'Sign Out');
+    });
+  });
+});
